70323879a2d9af8cbf945079f7b82a4757b196ed
Miguel Melo <miguelbritomelo@gmail.com>
2016-03-15 18:06:54 +0000

    [WIP] minor corrections to image lazyloading

--- src/scripts/lazyload.js
+++ src/scripts/lazyload.js
@@ -1,6 +1,14 @@
+// timeoutDuration
+const timeoutDuration = 100;
+
 class LazyLoad {
   constructor() {
-    this.images = document.querySelectorAll('[class="lazy-load"]');
+    this.images = document.getElementsByClassName('lazy-load');
+    this.images = Array.prototype.slice.call(this.images);
+
+    this._addEventListeners = this._addEventListeners.bind(this);
+    this._testLazyLoad = this._testLazyLoad.bind(this);
+
     this._addEventListeners();
     this.timmer = null;
 
@@ -8,6 +16,12 @@ class LazyLoad {
     this._testLazyLoad();
   }
 
+  /**
+  * _addEventListeners - inital event listener's binding
+  *
+  * @private
+  * @function
+  */
   _addEventListeners() {
     window.addEventListener('scroll', () => {
       if (this.timmer) {
@@ -17,16 +31,26 @@ class LazyLoad {
         requestAnimationFrame(() => {
           this._testLazyLoad();
         });
-      }, 100);
+      }, timeoutDuration);
     });
   }
 
+  /**
+  * _testLazyLoad - cicle through all images and check if they're inside the
+  * viewport or not (in order to show them)
+  *
+  * @private
+  * @function
+  */
   _testLazyLoad() {
     const wHeight = window.innerHeight;
+    let images = this.images;
+    let i = images.length - 1;
 
-    for (let i = this.images.length - 1; i >= 0; i--) {
-      if (this.images[i].getBoundingClientRect().top < wHeight) {
-        this.images[i].src = this.images[i].dataset.src;
+    for (; i >= 0; i--) {
+      if (images[i].getBoundingClientRect().top < wHeight) {
+        images[i].src = images[i].dataset.src; // this is where the magic happens
+        this.images.splice(i, 1); //make the list tiny as we chug along
       }
     }
   }
